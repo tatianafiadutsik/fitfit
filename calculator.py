68da07e403d7e725592dd6c5098945464794229d
Tatyana Fedutik <tanyushka_s@bk.ru>
2023-06-28 19:35:29 +0300

    Реализовано деление Task-2

--- calculator.py
+++ calculator.py
@@ -1,9 +1,19 @@
+import traceback
+
 class Calculator:
     last_res = None
 
     def sum(self, n1, n2):
         self.last_res = n1 + n2
         return n1 + n2
+
+    def divide(self, n1, n2):
+        try:
+            res = n1 / n2
+            self.last_res = res
+            return res
+        except:
+            traceback.print_exc()
 
     def print_last_res(self):
         print(self.last_res)
