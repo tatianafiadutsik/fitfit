7446ad8e434b50ec0d119c653e6292507eed8ee3
Tatyana Fedutik <tanyushka_s@bk.ru>
2023-06-28 19:15:57 +0300

    Задания

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,10 @@
 from calculator import Calculator
 
+
+# Задание
+# 1. Разработчику 1 - реализовать функцию умножения
+# 2. Разработчику 2 - реализовать функцию деления
+
 calc = Calculator
 calc.sum(10, 5)
 calc.print_last_res()
